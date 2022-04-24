12a058c69061cf68f9bd6ffc7644b25aacd52f56
Marco Maes <marco.maes@polteq.com>
2022-04-24 21:37:10 +0200

    Initial commit

---
+++ src/test/java/ExampleSteps.java
@@ -0,0 +1,8 @@
+import io.cucumber.java.en.Given;
+
+public class ExampleSteps {
+    @Given("I have a working test")
+    public void iHaveAWorkingTest() {
+        assert true;
+    }
+}
---
+++ src/test/resources/example.feature
@@ -0,0 +1,4 @@
+Feature: Example feature
+
+  Scenario: Example scenario
+    Given I have a working test
